058be5277afc4695e601ea840381fd2b8bdf576a
Jorge Yau <jorgeyau@gmail.com>
2010-10-13 12:36:38 -0500

    Localized footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <div id="footer">
 <div class="grid_24">
 <div class="copyright">
-<p><span><strong>Copyright &copy; <?php echo date("Y")." "; ?> <a href="http://geekvoice.com">geekvoice</a>.</strong> Ningun derecho reservado.</span></p>
+<p><span><strong><? _e("Copyright &copy;"); ?> <?php echo date("Y")." "; ?> <a href="http://geekvoice.com">geekvoice</a>.</strong> <?= _e("No rights reserved"); ?>.</span></p>
 </div><!-- .copyright -->
 <?php do_action('wp_footer'); ?>
 </div><!-- .grid_24 -->
